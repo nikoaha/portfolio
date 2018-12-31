0ebda29016fcf26761daaf07c01c20f84873a421
nikoaha <34025690+nikoaha@users.noreply.github.com>
2018-12-31 11:33:30 +0200

    Create Main.java

---
+++ Battlerobot/Main.java
@@ -0,0 +1,13 @@
+// The program activates from here.
+// This main opens the motors.
+
+package REXA;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Case muuttuja = new Case();
+		muuttuja.myCase();
+	}
+
+}
